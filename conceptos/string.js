e0a35e567d50e7446b2d79fa5038a47fc3111a4c
JuanJavier <juanjcbreton@gmail.com>
2024-09-12 11:52:28 -0600

    4.1 Strings Actualización

--- conceptos/string.js
+++ conceptos/string.js
@@ -10,3 +10,14 @@ console.log(message);
 // Template literals
 message = `おはよう。これは${ name }です。${ city }から来ました。`;
 console.log(message);
+
+// Separador
+console.log("-----------------");
+
+// Funciones métodos para strings
+console.log(message.toLowerCase());
+console.log(message.length);
+console.log(message.toUpperCase());
+console.log(message.includes('from'));
+console.log(message.startsWith('Hello'));
+console.log(message.endsWith('.'));
