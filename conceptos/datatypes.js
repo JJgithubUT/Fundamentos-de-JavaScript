8fbf34c17ce3b4a7291ba437573f4edadadaf7ec
JuanJavier <juanjcbreton@gmail.com>
2024-09-12 11:16:32 -0600

    02 variables

---
+++ conceptos/datatypes.js
@@ -0,0 +1,18 @@
+console.log(24);
+console.log(typeof 24);
+console.log(2.3);
+console.log(typeof 2.3);
+console.log(false);
+console.log(typeof false);
+console.log("Hola mundo!");
+console.log(typeof "Hola mundo!");
+console.log("c");
+console.log(typeof "c");
+console.log(null);
+console.log(typeof null);
+console.log([1,2,3]);
+console.log(typeof [1,2,3]);
+console.log({nombre: "Juan"});
+console.log(typeof {nombre: "Juan"});
+console.log(undefined);
+console.log(typeof undefined);
