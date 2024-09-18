c98656c24e4ffb479b5f2e37c17cbd7edd3cef28
JuanJavier <juanjcbreton@gmail.com>
2024-09-18 16:23:14 -0600

    06 Retos(Cubo)

---
+++ retos/reto_06.js
@@ -0,0 +1,26 @@
+function createCube(size) {
+  let cubo = '';
+
+  for (let i = 1; i <= size; i++) {
+    linea = '';
+    let pinzas = '/\\'.repeat(i);
+    let otroLado = '_\\'.repeat(size);
+    espaciado = ' '.repeat(size - i);
+    linea = espaciado + pinzas + otroLado;
+    cubo = cubo + linea + '\n'
+  }
+
+  for (let i = size; i >= 1; i--){
+    linea = '';
+    let pinzas = '\\/'.repeat(i);
+    let otroLado = '_/'.repeat(size);
+    espaciado = ' '.repeat(size - i);
+    linea = espaciado + pinzas + otroLado;
+    cubo = cubo + linea + '\n'
+  }
+
+  return cubo;
+}
+
+const cubeOfOne = createCube(5);
+console.log(cubeOfOne);
